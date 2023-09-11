6c99bd9fb2d5d35c3a9d3bcf69c1091d9b47eb28
nikitkachan <nikyc777@gmail.com>
2023-09-11 14:24:25 +0300

    сделал красиво

--- src/cat-api.js
+++ src/cat-api.js
@@ -1,14 +1,18 @@
+import axios from "axios";
+axios.defaults.headers.common["x-api-key"] = "live_Yz6vXkYOnJJkH7U2a4lVJC2KXu1xfs21U0nx7WZgaaB75k5jzunuj0MclPThNE0T";
+
 export function fetchBreeds() {
-    return fetch("https://api.thecatapi.com/v1/breeds")
-        .then(response => response.json())
+    return axios('https://api.thecatapi.com/v1/breeds')
+        .then(response => response.data)
+  
 };
 
 export function fetchCatImgByBreed(breedId) {
-    return fetch(`https://api.thecatapi.com/v1/images/search?breed_ids=${breedId}`)
-        .then(response => response.json())
+    return axios(`https://api.thecatapi.com/v1/images/search?breed_ids=${breedId}`)
+        .then(response => response.data)
 };
 
 export function fetchCatInfoByBreed(breedId) {
-    return fetch(`https://api.thecatapi.com/v1/breeds/${breedId}`)
-        .then(response => response.json())
+    return axios(`https://api.thecatapi.com/v1/breeds/${breedId}`)
+        .then(response => response.data)
 };
--- src/index.js
+++ src/index.js
@@ -2,9 +2,7 @@ import { fetchBreeds } from "./cat-api";
 import { fetchCatImgByBreed } from "./cat-api";
 import { fetchCatInfoByBreed } from "./cat-api";
 import Notiflix from 'notiflix';
-
-import axios from "axios";
-axios.defaults.headers.common["x-api-key"] = "live_Yz6vXkYOnJJkH7U2a4lVJC2KXu1xfs21U0nx7WZgaaB75k5jzunuj0MclPThNE0T";
+import SlimSelect from 'slim-select';
 
 const selectEl = document.querySelector(".breed-select");
 const loaderMsg = document.querySelector(".loader");
@@ -18,20 +16,18 @@ catInfo.style.alignItems = "start";
 catInfo.style.gap = "16px";
 catInfo.style.marginTop = "16px"
 
-
-
-
 fetchBreeds()
     .then(res => res.map(item => `<option value="${item.id}">${item.name}</option>`).join(""))
     .then(res => {
         selectEl.insertAdjacentHTML("beforeend", res);
-        selectEl.style.display = "block";
+        selectEl.style.display = "flex";
+        selectEl.style.width = "300px";
         loaderMsg.style.display = "none";
+        new SlimSelect({
+        select: document.querySelector('#selectElement')
+})
     })
-    .catch(error => {
-        loaderMsg.style.display = "none";
-        Notiflix.Notify.failure('Oops! Something went wrong! Try reloading the page!');
-    })
+    .catch(onFetchError)
 
 selectEl.addEventListener("change", onSelectChange);
 
@@ -40,7 +36,6 @@ function onSelectChange(e) {
     loaderMsg.style.display = "block";
     catInfo.style.display = "none";
     e.preventDefault;
-    console.dir(e.target.value);
     const selectedBreed = e.target.value;
     fetchCatImgByBreed(selectedBreed)
         .then(res => `<img src="${res[0].url}" width="600"/>`)
@@ -49,22 +44,19 @@ function onSelectChange(e) {
             loaderMsg.style.display = "none";
             errorMsg.style.display = "none";
         })
-        .catch(error => {
-            loaderMsg.style.display = "none";
-            Notiflix.Notify.failure('Oops! Something went wrong! Try reloading the page!');
-            catInfo.style.display = "none";
-        });
+        .catch(onFetchError);
     fetchCatInfoByBreed(selectedBreed)
         .then(res => `<div><h1>${res.name}</h1><p>${res.description}</p><p><b>Temperament: </b>${res.temperament}</p></div>`)
         .then(res => {
             catInfo.insertAdjacentHTML("beforeend", res);
             catInfo.style.display = "flex"
         })
-        .catch(error => {
-            loaderMsg.style.display = "none";
-            Notiflix.Notify.failure('Oops! Something went wrong! Try reloading the page!');
-            catInfo.style.display = "none";
-        });
+        .catch(onFetchError);
 };
 
-
+function onFetchError(error) {
+    Notiflix.Notify.failure('Oops! Something went wrong! Try reloading the page!');
+    catInfo.style.display = "none";
+    loaderMsg.style.display = "none";
+    console.error(error);
+}
